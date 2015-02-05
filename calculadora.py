f89354e0d6746f5cc695360e1d2d51b6a8aadeb9
ismael solanilla prieto <isolanil@lambda19.aulas.gsyc.es>
2015-02-05 09:42:19 +0100

    script final pasado el pep8

--- calculadora.py
+++ calculadora.py
@@ -7,69 +7,61 @@ Ejecución python calculadora.py funcion operando1 operando2
 
 
 import sys
-import os 
+import os
 
-functions = ['sum','rest','mult','div']
+functions = ['sum', 'rest', 'mult', 'div']
 
 if len(sys.argv) != 4:
 
-	sys.exit("Error en el número de argumentos")
+    sys.exit("Error en el número de argumentos")
 
-def mysum (parametro1,parametro2):
-	
-	return( parametro1 + parametro2) 
 
-def myrest (parametro1,parametro2):
+def mysum(parametro1, parametro2):
 
-	return (parametro1 - parametro2)
+    return(parametro1 + parametro2)
 
-def mymult (parametro1,parametro2):
 
-	return (parametro1 * parametro2)
+def myrest(parametro1, parametro2):
 
-def mydiv (parametro1,parametro2):
-	try:
-		return(parametro1/parametro2)
-	except ZeroDivisionError:
-		print("No se puede dividir entre 0")
-		exit()
+    return(parametro1 - parametro2)
+
+
+def mymult(parametro1, parametro2):
+
+    return(parametro1 * parametro2)
+
+
+def mydiv(parametro1, parametro2):
+    try:
+        return(parametro1/parametro2)
+    except ZeroDivisionError:
+        print("No se puede dividir entre 0")
+        exit()
 
 try:
-	int(sys.argv[2])
-	int(sys.argv[1])
+    float(sys.argv[3])
+    float(sys.argv[2])
 except ValueError:
-	print('could not convert string to float')
-	exit()
-	
-print ('function: ' + sys.argv[1] + ' operando1: ' + sys.argv[2] + ' operando2: ' + sys.argv[3])
+    print('could not convert string to float')
+    exit()
 
 
+if sys.argv[1] == functions[0]:
 
-if sys.argv[1] == functions[0] :
-
-	print mysum(float(sys.argv[2]),float(sys.argv[3]))
+    print mysum(float(sys.argv[2]), float(sys.argv[3]))
 
 elif sys.argv[1] == functions[1]:
-	
-	print myrest(float(sys.argv[2]),float(sys.argv[3]))
+
+    print myrest(float(sys.argv[2]), float(sys.argv[3]))
 
 elif sys.argv[1] == functions[2]:
-	
-	print mymult(float(sys.argv[2]),float(sys.argv[3]))
+
+    print mymult(float(sys.argv[2]), float(sys.argv[3]))
 
 elif sys.argv[1] == functions[3]:
-	
-	print mydiv(float(sys.argv[2]),float(sys.argv[3]))
+
+    print mydiv(float(sys.argv[2]), float(sys.argv[3]))
 
 else:
-	
-	print('funcion incorrecta ' + '|' + sys.argv[1] + '|')
 
-
-
-
-
-
-
-
-
+    print('funcion incorrecta ' + '|' + sys.argv[1] + '|')
